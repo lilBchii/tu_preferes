7cf6a2b849c1391b82b11151cc34a0a04673e7b5
lilBchii <104091142+lilBchii@users.noreply.github.com>
2023-06-16 14:40:51 +0200

    Update get_items.php

--- get_items.php
+++ get_items.php
@@ -2,77 +2,83 @@
     
     include_once 'database.php';
 
-    // Calculate elo of each choices after a match
+    // Calculate elo of each choices after a choice
     function compute_elo($winner, $loser) {
-
-        global $new_score_win, $new_score_los;
         
         $dif = abs($winner - $loser);
-        $g_var = (40+($dif**1.5)/10)**0.5;
+        $g_var = (40+($dif**1.5)/10)**(1/1.3);
         $h_var = (10/(1+$dif**0.5))*$g_var;
-        $l_var=(1/(1+$dif**3))*$g_var;
+        $l_var=(1/(1+$dif**1.5))*$g_var;
 
         if ($winner < $loser) {
             $new_score_win = round($winner + $h_var);
             $new_score_los = round($loser - $h_var);
-        } elseif ($winner > $loser) {
+        } elseif ($winner >= $loser) {
             $new_score_win = round($winner + $l_var);
             $new_score_los = round($loser - $l_var);
-        } elseif ($dif <= $g_var) {
-            $new_score_win = round($winner + $l_var);
-            $new_score_los = round($loser);
         }
+
+        return [$new_score_win, $new_score_los];
     }
 
-    function gen_match($conn) {
-
-        global $left_score, $right_score, $left_choice, $right_choice;
-
-        // Get the available choices and their scores from SQL db
-        $get_choices = mysqli_query($conn, "SELECT choice FROM items ORDER BY id ASC") or die(mysqli_error());
-        $get_scores = mysqli_query($conn, "SELECT score FROM items ORDER BY id ASC") or die(mysqli_error());
-
-        // Fill arrays with choices and scores
-        $items = array();
-        for($i = 0; $items[$i] = mysqli_fetch_assoc($get_choices); $i++);
-        array_pop($items);
-        $scores = array();
-        for($i = 0; $scores[$i] = mysqli_fetch_assoc($get_scores); $i++);
-        array_pop($scores);
-
-        // Choose two random items
-        $choices = array_rand($items, 2);
-
-        // Get corresponding choices
-        $left_choice = implode($items[$choices[0]]);
-        $right_choice = implode($items[$choices[1]]);
-
-        // Get corresponding scores
-        $left_score = (int) implode($scores[$choices[0]]);
-        $right_score = (int) implode($scores[$choices[1]]);
-
-        echo $left_choice;
-        echo $left_choice;
-        echo $right_choice;
+    // Get the items from SQL db
+    $list_items = mysqli_query($conn, "SELECT * FROM items ORDER BY id ASC");
+    
+    // Fetch array
+    while($row = mysqli_fetch_array($list_items)){
+        $tab_list_items[] = $row;
     }
 
-    gen_match($conn);
+    // Choose two random ids
+    $chosen = array_rand($tab_list_items, 2);
+
+    var_dump($chosen);
+
+    // Get ids
+    $left_id = $tab_list_items[$chosen[0]]['id'];
+    $right_id = $tab_list_items[$chosen[1]]['id'];
+
+    // Get scores
+    $left_score = $tab_list_items[$chosen[0]]['score'];
+    $right_score = $tab_list_items[$chosen[1]]['score'];
+
+    // Get Strings
+    $left_choice = $tab_list_items[$chosen[0]]['choice'];
+    $right_choice = $tab_list_items[$chosen[1]]['choice'];
 
     // Behavior when buttons are clicked
     // Left one
     if (isset($_POST['left'])) {
-        compute_elo($left_score, $right_score);
-        mysqli_query($conn, "UPDATE items SET score = '$new_score_win' WHERE choice = '$left_choice'");
-        mysqli_query($conn, "UPDATE items SET score = '$new_score_los' WHERE choice = '$right_choice'");
+
+        $left_id = $_POST['info_left'];
+        $right_id = $_POST['info_right'];
+        $left_score = $_POST['info_left_score'];
+        $right_score = $_POST['info_right_score'];
+
+        $scores = compute_elo($left_score, $right_score);
+        $winner_score = $scores[0];
+        $loser_score = $scores[1];
+
+        mysqli_query($conn, "UPDATE items SET score = '$winner_score' WHERE id = '$left_id'");
+        mysqli_query($conn, "UPDATE items SET score = '$loser_score' WHERE id = '$right_id'");
         header("Location: /index.php");
         exit();
     }
 
     // Right one 
     if (isset($_POST['right'])) {
-        compute_elo($right_score, $left_score);
-        mysqli_query($conn, "UPDATE items SET score = '$new_score_los' WHERE choice = '$left_choice'");
-        mysqli_query($conn, "UPDATE items SET score = '$new_score_win' WHERE choice = '$right_choice'");
+
+        $left_id = $_POST['info_left'];
+        $right_id = $_POST['info_right'];
+        $left_score = $_POST['info_left_score'];
+        $right_score = $_POST['info_right_score'];
+
+        $scores = compute_elo($right_score, $left_score);
+        $winner_score = $scores[0];
+        $loser_score = $scores[1];
+
+        mysqli_query($conn, "UPDATE items SET score = '$winner_score' WHERE id = '$right_id'");
+        mysqli_query($conn, "UPDATE items SET score = '$loser_score' WHERE id = '$left_id'");
         header("Location: /index.php");
         exit();
     }
